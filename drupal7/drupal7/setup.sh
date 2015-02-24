e4740357fbf5902ea5a4b9fdf2962fa1fbee233d
Sebastian Gilits <sep.gil@gmail.com>
2015-02-24 10:41:51 +0100

    Replaced nginx+fpm paths with a soft link and removed the settings.php creation out of enviroment variables for the drupal7 image.

--- drupal7/drupal7/setup.sh
+++ drupal7/drupal7/setup.sh
@@ -7,35 +7,7 @@ pear install drush/drush
 # Download drupal to the /var/www folder.
 drush dl drupal --destination=/var --drupal-project-rename=www -y
 
-# Create a settings file based on the enviroment variables.
-$settings=/var/www/sites/default/settings.php
-touch $settings
-chmod a+w $settings
+# Create a settings file.
+cp /var/www/sites/default/default.settings.php /var/www/sites/default/settings.php
 
-# Set some default values.
-if [ -z $MYSQL_DATABASE]; then
-    export MYSQL_DATABASE=""
-fi
-
-if [ -z $MYSQL_USER]; then
-    export MYSQL_USER=$MYSQL_DATABASE
-fi
-
-if [ -z $MYSQL_PASSWORD]; then
-    export MYSQL_PASSWORD=""
-fi
-
-if [ -z $MYSQL_PORT_3306_TCP_ADDR]; then
-    export MYSQL_PORT_3306_TCP_ADDR="localhost"
-fi
-
-# Write the settings into the settings.php
-cat <<EOT >> $settings
-\$database['default']['default'] = array(
-  'driver' => 'mysql',
-  'database' => '$MYSQL_DATABASE',
-  'username' => '$MYSQL_USER',
-  'password' => '$MYSQL_PASSWORD',
-  'host' => '$MYSQL_PORT_3306_TCP_ADDR',
-);
-EOT
+chown -R www-data:www-data /var/www
